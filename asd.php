6452363a342aa6b7b4f44f718905b46bcc271313
kaan <kaankasatura45gmail.com>
2018-07-11 15:58:53 +0300

    step2

--- asd.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db= new PDO("mysql:host=localhost;dbname=deneme",'root','');
-$guncelle = $db->prepare("update deneme set deger=? where id=1");
-$ıslem = $guncelle->execute(array("18"));
-
-foreach ($db->query("SELECT * FROM deneme") as $yaz) {
-	echo $yaz['deger']; echo "<br>";
-}
-?>
